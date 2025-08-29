c5002bd99d9b9f710ba06f2401a3e8a0c9892cce
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-08-28 22:27:42 -0300

    Update SecurityConfig.java

--- sistemalogin/src/main/java/com/gustavo/sistemalogin/security/SecurityConfig.java
+++ sistemalogin/src/main/java/com/gustavo/sistemalogin/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Desabilita CSRF, comum para APIs REST
+                .csrf(AbstractHttpConfigurer::disable) // Desabilita CSRF, comum para APIs REST
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/users/register", "/api/users/login", "/api/users/forgot-password", "/api/users/reset-password").permitAll() // Permite acesso público a estes endpoints
                         .anyRequest().authenticated() // Exige autenticação para qualquer outro request
@@ -23,7 +24,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // ADICIONE ESTE MÉTODO ABAIXO
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
