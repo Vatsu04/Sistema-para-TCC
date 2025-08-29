61f683f78e2cca146f028910125be8529e0c02fa
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-08-29 10:24:37 -0300

    Update SecurityConfig.java

--- sistemalogin/src/main/java/com/gustavo/sistemalogin/config/SecurityConfig.java
+++ sistemalogin/src/main/java/com/gustavo/sistemalogin/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.gustavo.sistemalogin.security;
+package com.gustavo.sistemalogin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
