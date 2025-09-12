0cb15587e995af6129eae81d49fac87b0548a1cc
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-09-12 19:06:33 -0300

    Update

--- sistemalogin/src/main/java/com/gustavo/sistemalogin/dto/FunilResponseDTO.java
+++ sistemalogin/src/main/java/com/gustavo/sistemalogin/dto/FunilResponseDTO.java
@@ -5,16 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class FunilResponseDTO {
 
     private Long id;
     private String nome;
 
-    public FunilResponseDTO(Funil funil) {
-        this.id = funil.getId();
-        this.nome = funil.getNome();
+
+
+
+
+    public FunilResponseDTO(Object o) {
     }
 }
--- sistemalogin/src/main/java/com/gustavo/sistemalogin/repository/FunilRepository.java
+++ sistemalogin/src/main/java/com/gustavo/sistemalogin/repository/FunilRepository.java
@@ -3,8 +3,11 @@ package com.gustavo.sistemalogin.repository;
 import com.gustavo.sistemalogin.model.Funil;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface FunilRepository extends JpaRepository<Funil, Long> {
     List<Funil> findByUserId(Long userId);
+
+    Collection<Object> findByUserEmail(String userEmail);
 }
