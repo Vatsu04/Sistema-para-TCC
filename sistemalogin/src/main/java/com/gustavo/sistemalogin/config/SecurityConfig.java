9363cbe8d9021204211b574d435a59f5c3d8aca6
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-10-07 16:20:08 -0300

    ApplicationConfig

--- sistemalogin/src/main/java/com/gustavo/sistemalogin/config/SecurityConfig.java
+++ sistemalogin/src/main/java/com/gustavo/sistemalogin/config/SecurityConfig.java
@@ -48,11 +48,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
