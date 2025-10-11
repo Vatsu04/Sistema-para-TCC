fbda1cb3773151fbc0b7b3d57b0182321c1fdbdb
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-10-11 12:55:46 -0300

    Regras de Senha

--- sistemalogin/src/main/java/com/gustavo/sistemalogin/dto/UserUpdateDTO.java
+++ sistemalogin/src/main/java/com/gustavo/sistemalogin/dto/UserUpdateDTO.java
@@ -1,18 +1,25 @@
 package com.gustavo.sistemalogin.dto;
 
 import com.gustavo.sistemalogin.model.enums.PerfilUsuario;
+import com.gustavo.sistemalogin.validation.Password; // <-- IMPORTE A ANOTAÇÃO
 
 public class UserUpdateDTO {
 
+    @Password // <-- ADICIONE A ANOTAÇÃO AQUI
     private String senha;
-    private Boolean ativo;
 
+    private Boolean ativo;
     private PerfilUsuario perfil;
 
 
 
+    // Getters e Setters
     public String getSenha() {
         return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
     public Boolean getAtivo() {
@@ -29,9 +36,5 @@ public class UserUpdateDTO {
 
     public void setPerfil(PerfilUsuario perfil) {
         this.perfil = perfil;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
     }
 }
